125c020075df83ba8bf9558e8ae9a21e7e6442ed
Larry <larry@192-168-1-10.tpgi.com.au>
2025-01-18 17:17:57 +1100

    courseinfo step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,19 +4,21 @@ const Header = ({ course }) => {
   )
 }
 
+const Part = ({ part, exercise }) => {
+  return (
+    <div>
+      <p>{part} {exercise}</p>
+    </div>
+  )
+  }
+
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
 
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+        <Part part={part1} exercise={exercises1} />
+        <Part part={part2} exercise={exercises2} />
+        <Part part={part3} exercise={exercises3} />
     </div>
   )
 }

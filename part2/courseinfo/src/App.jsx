06588a8d7f375267e232b4e9435bd73a812873e2
Larry <larry@192-168-1-4.tpgi.com.au>
2025-02-18 14:20:19 +1100

    courseinfo part 2 step 9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -14,41 +14,77 @@ const Part = ({ part }) => (
     </p>
 )
 
+const Total = ({ course }) => {
+  const sumOfExercises = course.parts.reduce((accumulator, currentValue) => 
+    accumulator + currentValue.exercises, 0
+  ) 
+
+  return <p><strong>Total of {sumOfExercises} exercises</strong></p>
+}
+
 const Course = ({ course }) => {
 
   return (
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course ={course} />
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <Course course={course} />
+    courses.map((courseItem) => 
+    (
+      <Course key={courseItem.id} course={courseItem} />
+    )
+    )
   )
 
 }

95b7a23fbc8d0c2d0b31e4a55009cca9ddfdc686
Larry <larry@192-168-1-4.tpgi.com.au>
2025-02-20 14:17:14 +1100

    phonebook part 2 step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '555-55-5555' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const nameExists = () => 
@@ -16,12 +21,15 @@ const App = () => {
   const handleSubmit = (event) => {
     event.preventDefault()
     
-    const newPersonObject = { name: newName }
-    
+    const newPersonObject = { name: newName, number: newNumber }
+
     nameExists() 
       ? alert(`${newName} is already added to phonebook`) 
       : setPersons(persons.concat(newPersonObject))
+    
     setNewName('')
+    setNewNumber('')
+    document.querySelector('input[name="name"]').focus();
   }
 
   return (
@@ -29,7 +37,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={handleChange} value={newName} autoFocus />
+          name: <input name="name" onChange={handleNameChange} value={newName} autoFocus />
+        </div>
+        <div>
+          number: <input name="number" onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -38,7 +49,7 @@ const App = () => {
       <h2>Numbers</h2>
         <div>
           {persons.map(person => 
-            <div key={person.name}>{person.name}</div>
+            <div key={person.name}>{person.name} {person.number}</div>
             )}
         </div>
     </div>

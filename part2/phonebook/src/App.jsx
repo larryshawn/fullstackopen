fa5d22cf45efdcc88fa562251363b3dff90bc956
Larry <larry@192-168-1-9.tpgi.com.au>
2025-03-05 13:46:12 +1100

    phonebook part 2 step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import SearchFilter from './components/SearchFilter'
 import Form from './components/Form'
 import PhonebookEntries from './components/PhonebookEntries'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
   const [filteredPersons, setFilteredPersons] = useState([])
+
+useEffect(() => {
+  axios.get('http://localhost:3001/persons')
+  .then((response) => { 
+    setPersons(response.data)
+  })
+}, [])
+
 
   const handleNameChange = (event) => setNewName(event.target.value)
 
